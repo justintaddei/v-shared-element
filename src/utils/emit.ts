540e1683fa8cd1444522817f8863d1c4dba52f6e
Justin Taddei <justin@justintaddei.com>
2020-06-06 14:48:39 -0500

    Clear branch

--- src/utils/emit.ts
+++
@@ -1,9 +0,0 @@
-import { VNode } from 'vue/types/vnode'
-
-/**
- *  Helper function to emit vue compatible events
- */
-export function $emit(vnode: VNode, name: string, data: any) {
-  const handlers = vnode.data?.on ?? vnode.componentOptions?.listeners
-  handlers?.[name].fns(data)
-}
--- src/utils/withinViewport.ts
+++
@@ -1,5 +0,0 @@
-import { IllusoryElement } from 'illusory/types'
-
-export function withinViewport({ rect }: IllusoryElement) {
-  return rect.bottom >= 0 && rect.right >= 0 && rect.top <= window.innerHeight && rect.left <= window.innerWidth
-}
